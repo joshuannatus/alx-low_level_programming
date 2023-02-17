d52382de18ca6ad2298e91c9f2a3b19e889fd7d8
joshuannatus <joshuannatus@gmail.com>
2023-02-16 17:04:01 -0800

    Betty documented c programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-/* This is a C program that uses puts*/
+/**
+ * main - This is a C program that uses puts fuction
+ *
+ *Return: 0 (success)
+ **/
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
