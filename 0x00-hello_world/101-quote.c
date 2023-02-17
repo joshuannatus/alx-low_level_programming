d3757f21737b4ca82e867edfd0c1f4faebb9df70
joshuannatus <joshuannatus@gmail.com>
2023-02-16 19:16:02 -0800

    Advance task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * main - UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity
+ * 
+ *
+ * Return: Always 0
+ **/
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
