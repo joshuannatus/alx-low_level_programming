1c75cf8e7b066dbb98d3f520a4d4c144070b46ac
joshuannatus <joshuannatus@gmail.com>
2023-03-02 20:51:24 -0800

    strncat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * *_strcat - This program concatenates two strings
+ * * @dest: parameter pointer to a char
+ * @src: parameter  pointer to a char
+ * Return: return value of dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int a;
+	int b;
+
+	a = 0;
+	b = 0;
+
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
+
+	while (src[b] != '\0')
+	{
+		dest[a] = src[j];
+		b++;
+		a++;
+	}
+
+	dest[a] = '\0';
+	return (dest);
+}
