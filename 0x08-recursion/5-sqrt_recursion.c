3ed7979004aadc7ce8957fb19180da73f4dc5f64
joshuannatus <joshuannatus@gmail.com>
2023-03-08 17:30:47 -0800

    recursion task

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * pow_operation - returns the natural square root of a number.
+ * @n: input number.
+ * @i: iterator.
+ * Return: square root or -1.
+ */
+int pow_operation(int n, int i)
+{
+	if (i % (n / i) == 0)
+	{
+		if (i * (n / i) == n)
+			return (i);
+		else
+			return (-1);
+	}
+	return (0 + pow_operation(n, i + 1));
+}
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: input number.
+ * Return: natural square root.
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (0);
+	if (n == 1)
+		return (1);
+	return (pow_operation(n, 2));
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+This is the repository for All task in respect to C programming Language in my journey to becoming a Software Engineer
